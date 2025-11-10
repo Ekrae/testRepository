ca98d71bf783998c1ae5c5c51d8c70e2c4c83c97
Ekrae <sws10027@gmail.com>
2025-11-10 10:02:25 +0900

    파일 정리함

--- src/week09/FileManager.java
+++ src/week09/FileManager.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package week09;
 
 import java.io.File;
@@ -20,20 +15,18 @@ public class FileManager {
     }
 
     public static void fileMerge(String filename1, String filename2, String filename3) {
-        try {
-            try (
-                    Scanner file1 = new Scanner(new File(filename1));
-                    Scanner file2 = new Scanner(new File(filename2));
-                    PrintWriter outfile = new PrintWriter(filename3);
-            ) {
+        try(Scanner file1 = new Scanner(new File(filename1));
+            Scanner file2 = new Scanner(new File(filename2));
+            PrintWriter outfile = new PrintWriter(filename3)){
+
+
+
                 fileWriter(file1, outfile);
                 fileWriter(file2, outfile);
                 System.out.println("파일 합치기가 완료되었습니다.");
-            }
-
         } catch (FileNotFoundException e) {
             System.out.println("파일이 없음");
-            throw new RuntimeException(e);
+            //throw new RuntimeException(e);
         }
     }
 }
--- week09/MenuRangeCheckException.java
+++
@@ -1,8 +0,0 @@
-package week09;
-
-public class MenuRangeCheckException extends RuntimeException{
-    public MenuRangeCheckException(String message) {
-        //번개모양이 나오네
-        super(message);
-    }
-}
